ce4459769abb672b59ab3a870c95f64ac4cd9616
rafaelswr <rafaels.borges91@gmail.com>
2024-11-22 17:38:01 +0000

    Set up Base project

---
+++ src/main/java/com/rafaelswr/springsecurityindeep/SpringSecurityinDeepApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.springsecurityindeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityinDeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityinDeepApplication.class, args);
+    }
+
+}
