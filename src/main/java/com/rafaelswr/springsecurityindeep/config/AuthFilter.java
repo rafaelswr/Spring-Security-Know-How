9ca4a487f34e7eb77c2f9f89a3ca3ac6a8e86f18
rafaelswr <rafaels.borges91@gmail.com>
2024-11-23 19:44:36 +0000

    Custom security filter chain for endpoint authorization
    - Implemented security filter chain demo to configure authentication and authorization for endpoints;

---
+++ src/main/java/com/rafaelswr/springsecurityindeep/config/AuthFilter.java
@@ -0,0 +1,37 @@
+package com.rafaelswr.springsecurityindeep.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthFilter {
+
+    private final AuthProviderConf authenticationProvider;
+
+    @Autowired
+    public AuthFilter(AuthProviderConf authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.httpBasic(Customizer.withDefaults());
+
+            httpSecurity.authorizeHttpRequests(request->{
+                        request.requestMatchers("/hello").authenticated();
+                        request.requestMatchers("/other").hasRole("read");
+                        request.anyRequest().permitAll();
+                    }).formLogin(Customizer.withDefaults());
+
+            
+            return httpSecurity.build();
+    }
+
+}
